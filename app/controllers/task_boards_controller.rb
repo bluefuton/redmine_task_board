10b769c980aef617ac4837eba05f3c3878acc49a
Chris Rosser <chris@bluefuton.com>
2011-07-21 18:32:10 +0100

    Task board tab now shows under project. You can select a version and view the issues - but it's not pulling the correct categorised issues yet

--- app/controllers/task_boards_controller.rb
+++ app/controllers/task_boards_controller.rb
@@ -1,8 +1,16 @@
 class TaskBoardsController < ApplicationController
   unloadable
 
+  def index
+    @project = Project.find(params[:project_id])
+  end
+  
   def show
     @version = Version.find(params[:id])
-    @issues = Issue.all(:conditions => {:fixed_version_id => @version.id})
+    @project = @version.project
+    @issues_to_do = Issue.all(:conditions => {:fixed_version_id => @version.id})
+    @issues_in_progress = Issue.all(:conditions => {:fixed_version_id => @version.id})
+    @issues_to_verify = Issue.all(:conditions => {:fixed_version_id => @version.id})
+    @issues_closed = Issue.all(:conditions => {:fixed_version_id => @version.id})
   end
 end
--- init.rb
+++ init.rb
@@ -3,8 +3,16 @@ require 'redmine'
 Redmine::Plugin.register :redmine_task_board do
   name 'Redmine Task Board plugin'
   author 'Chris Rosser'
-  description 'Provide an agile task board for a version'
+  description 'Provide a task board for a version'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  
+  permission :task_boards, {:task_boards => [:index, :show]}, :public => true
+  menu :project_menu, :task_boards, { :controller => 'task_boards', :action => 'index' }, :caption => 'Task board', :after => :activity, :param => :project_id
+
+  project_module :task_boards do
+    permission :view_task_board_versions, :task_boards => :index
+    permission :view_task_board, :task_boards => :vote
+  end
 end
