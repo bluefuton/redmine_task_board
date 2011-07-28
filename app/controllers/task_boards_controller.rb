ec3748e564db2ef50cfa933511a955ef9a976fe9
Chris Rosser <chris@bluefuton.com>
2011-07-28 09:32:30 +0100

    Updated status names to match the ones used at Republic. I'll make this configurable in a later release.

--- app/controllers/task_boards_controller.rb
+++ app/controllers/task_boards_controller.rb
@@ -11,9 +11,9 @@ class TaskBoardsController < ApplicationController
     @project = @version.project
     
     @issues_to_do = Issue.find_by_version_id_and_status_names(@version.id, ['New'])
-    @issues_in_progress = Issue.find_by_version_id_and_status_names(@version.id, ['In Progress', 'Feedback'])    
-    @issues_to_verify = Issue.find_by_version_id_and_status_names(@version.id, ['Resolved'])
-    @issues_closed = Issue.find_by_version_id_and_status_names(@version.id, ['Closed'])
+    @issues_in_progress = Issue.find_by_version_id_and_status_names(@version.id, ['In Progress', 'Feedback Requested'])    
+    @issues_to_verify = Issue.find_by_version_id_and_status_names(@version.id, ['Completed'])
+    @issues_closed = Issue.find_by_version_id_and_status_names(@version.id, ['Rejected', 'Closed'])
   end
   
   protected
